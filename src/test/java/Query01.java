5bf4bffb80e2ce20c88f9f0478ac9523728ebdcd
ilker61 <jakyuer@gmail.com>
2022-07-27 12:20:28 +0200

    27.07.22 JDBC

--- src/test/java/Query01.java
+++ src/test/java/Query01.java
@@ -26,11 +26,19 @@ public class Query01 {
         // 5. adim; Sonuclari al
 
         while (veri.next()){
-            System.out.println(veri.getInt(1) + veri.getString(2) +
-                    veri.getString(3) + veri.getString(4) );
+            // 1. yol
+            // System.out.println(veri.getInt(1) + veri.getString(2) +
+                    // veri.getString(3) + veri.getString(4) );
+
+            // 2. yol
+            System.out.println(veri.getInt("okul_no") + veri.getString("ogrenci_ismi") +
+                    veri.getString("sinif") + veri.getString("cinsiyet") );
+
+           //3. yol
+            // System.out.printf("%-6d %-15.15s %-8s %-8s\n", veri.getInt(1) + veri.getString(2) +
+               //     veri.getString(3) + veri.getString(4));
+
         }
-
-
 
     }
 }
