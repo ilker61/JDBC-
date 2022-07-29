b21fc41a05109f738db3b65b4045e36b41824094
ilker61 <jakyuer@gmail.com>
2022-07-29 19:59:56 +0200

    29.07.22 JDBC

--- src/test/java/Query06.java
+++ src/test/java/Query06.java
@@ -1,6 +1,24 @@
-public class Query06 {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Query06 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimeZone=UTC","root","1234");
+
+        Statement st = con.createStatement();
+
+        // Soru; Bolumler tablosunda yani bir kayit (80, 'ARGE', 'ISTANBUL')
+
+        int s1 = st.executeUpdate("INSERT INTO bolumler VALUES (80, 'ARGE', 'ISTANBUL')");
+
+        // Not; executeUpdate etkilenen satir ayisini dondurur
+
+        System.out.println(s1+ "satir eklendi");
 
 
 
