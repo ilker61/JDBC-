086987e501b3706d8b8823b3539c16a84a911c7d
ilker61 <jakyuer@gmail.com>
2022-07-29 09:45:55 +0200

    29.07.22 JDBC

--- src/test/java/Query02.java
+++ src/test/java/Query02.java
@@ -23,6 +23,17 @@ public class Query02 {
                     veri.getString("sinif") + veri.getString("cinsiyet") );
         }
 
+        // SORU; Ogrenciler taplosundaki 9. sinif kiz ogrencilerin bilgilerini getir
+
+        System.out.println("===========================================================");
+
+        ResultSet veri2=st.executeQuery("select * from ogrenciler where cinsiyet='K' and sinif=9");
+
+        while (veri2.next()){
+            System.out.println(veri2.getInt("okul_no") + veri2.getString("ogrenci_ismi") +
+                    veri2.getString("sinif") + veri2.getString("cinsiyet") );
+        }
+
         con.close();
         st.close();
         veri.close();
