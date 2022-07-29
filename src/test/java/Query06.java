da9ce81170f94cfa07ffb65cce6ae1c03f4ed854
ilker61 <jakyuer@gmail.com>
2022-07-29 20:41:06 +0200

    29.07.22 JDBC

--- src/test/java/Query06.java
+++ src/test/java/Query06.java
@@ -14,13 +14,58 @@ public class Query06 {
 
         // Soru; Bolumler tablosunda yani bir kayit (80, 'ARGE', 'ISTANBUL')
 
-        int s1 = st.executeUpdate("INSERT INTO bolumler VALUES (80, 'ARGE', 'ISTANBUL')");
+        // int s1 = st.executeUpdate("INSERT INTO bolumler VALUES (80, 'ARGE', 'ISTANBUL')");
 
         // Not; executeUpdate etkilenen satir ayisini dondurur
 
-        System.out.println(s1+ "satir eklendi");
+        // System.out.println(s1+ "satir eklendi");
+
+        /*
+        String [] veri1={"INSERT INTO bolumler VALUES (95, 'YEMEKHANE', 'ISTANBUL')",
+                "INSERT INTO bolumler VALUES (85, 'OFIS', 'ANKARA')",
+                "INSERT INTO bolumler VALUES (75, 'OFIS','TRABZON')"};
+
+        int count=0;
+
+        for (String each: veri1
+             ) {
+        count=count + st.executeUpdate(each);
+        }
+        System.out.println(count + " data eklendi");
 
 
+
+        // 2.yol
+
+        String [] veri2={"INSERT INTO bolumler VALUES (81, 'YEMEKHANE', 'ISTANBUL')",
+                "INSERT INTO bolumler VALUES (82, 'OFIS', 'ANKARA')",
+                "INSERT INTO bolumler VALUES (83, 'OFIS','TRABZON')"};
+
+
+        for (String w: veri2
+             ) {
+            st.addBatch(w);  // yukaridaki verilerin tamamini bir araya topluyor
+        }
+
+        st.executeBatch();  // bir araya getirilen verileri tek seferde gonderiyor
+
+        System.out.println("Veriler database ye eklendi");
+
+        */
+
+
+        // Soru; Bolumler tablosuna birden fazla kayit ekleyelim.
+
+        String [] veri3={"INSERT INTO bolumler VALUES (100, 'YEMEKHANE', 'ISTANBUL')",
+                "INSERT INTO bolumler VALUES (101, 'OFIS', 'ANKARA')",
+                "INSERT INTO bolumler VALUES (102, 'OFIS','TRABZON')", "INSERT INTO bolumler VALUES (103, 'OFIS','TRABZON')",
+                "INSERT INTO bolumler VALUES (104, 'OFIS','TRABZON')"};
+
+        for (String e: veri3
+             ) {
+            st.addBatch(e);
+        }
+        st.executeBatch();
 
     }
 }
---
+++ src/test/java/Query07.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class Query07 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimeZone=UTC","root","1234");
+
+
+        // STATEMENT, PREPAREDSTATEMENT FARKI
+
+        // STATEMENT:
+
+        /*
+        Statement st = con.createStatement();
+        st.executeUpdate("INSERT INTO ogrenciler VALUES (200, 'Aali CANA', 10, 'E')");
+        */
+
+        // PREPAREDSTATEMENT; ifadeler parametlre ile yollanabilir
+
+        PreparedStatement ps= con.prepareStatement("INSERT INTO ogrenciler VALUES (?, ?, ?, ?)");
+        ps.setInt(1, 201);
+        ps.setString(2, "Veli Xan");
+        ps.setString(3,"12");
+        ps.setString(4,"E");
+
+        ps.executeUpdate();
+
+        System.out.println("Veri girisi saglandi ya laa");
+
+    }
+}
